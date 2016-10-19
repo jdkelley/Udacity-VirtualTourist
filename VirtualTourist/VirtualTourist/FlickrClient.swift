75ddb78d83e088d21306810037e6ec1045a1f3a6
Joshua Kelley <joshuadavidkelley@gmail.com>
2016-10-19 16:22:42 -0400

    Adds FlickrCLient Networking class.

---
+++ VirtualTourist/VirtualTourist/FlickrClient.swift
@@ -0,0 +1,15 @@
+//
+//  FlickrClient.swift
+//  VirtualTourist
+//
+//  Created by Joshua Kelley on 10/19/16.
+//  Copyright © 2016 Joshua Kelley. All rights reserved.
+//
+
+import Foundation
+
+class FlickrClient {
+    
+    
+    
+}
