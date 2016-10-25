cd6f13f862fe01cb995c2a80db0cf26ee917c346
Joshua Kelley <joshuadavidkelley@gmail.com>
2016-10-25 15:48:00 -0400

    Adds networking class

--- VirtualTourist/VirtualTourist/FlickrClient.Constants.swift
+++ VirtualTourist/VirtualTourist/FlickrClient.Constants.swift
@@ -7,3 +7,132 @@
 //
 
 import Foundation
+
+extension FlickrClient {
+
+    /// Constants relating to the FlickrClient Networking calls
+    ///
+    ///
+    /// Constants include:
+    ///  * `apiScheme` = "https"
+    ///  * `apiHost` = "api.flickr.com"
+    ///  * `apiPath` = "/services/rest"
+    ///  * `searchBBoxHalfWidth` = 1.0
+    ///  * `searchBBoxHalfHeight` = 1.0
+    ///  * `searchLatRange` = (-90.0, 90.0)
+    ///  * `SearchLonRange` = (-180.0, 180.0)
+    struct Constants {
+        
+        ///"https"
+        static let apiScheme = "https"
+        
+        /// "api.flickr.com"
+        static let apiHost = "api.flickr.com"
+        
+        /// "/services/rest"
+        static let apiPath = "/services/rest"
+
+        /// Double: 1.0
+        static let searchBBoxHalfWidth = 1.0
+        
+        /// Double: 1.0
+        static let searchBBoxHalfHeight = 1.0
+        
+        /// Tuple: (-90.0, 90.0)
+        static let searchLatRange = (-90.0, 90.0)
+        
+        /// Tuple: (-180.0, 180.0)
+        static let searchLonRange = (-180.0, 180.0)
+    }
+    
+    /// Constants that define url parameter keys.
+    ///
+    /// These keys include:
+    ///  * `method` = "method"
+    ///  * `apiKey` = "api_key"
+    ///  * `bbox` = "bbox"
+    ///  * `safeSearch` = "safe_search"
+    ///  * `lat` = "lat"
+    ///  * `lon` = "lon"
+    ///  * `extras` = "extras"
+    ///  * `format` = "format"
+    ///  * `nojsoncallback` = "nojsoncallback"
+    struct ParameterKeys {
+        
+        /// "method"
+        static let method = "method"
+        
+        /// "api_key"
+        static let apiKey = "api_key"
+        
+        /// "bbox"
+        static let bbox = "bbox"
+        
+        /// "safe_search"
+        static let safeSearch = "safe_search"
+        
+        /// "lat"
+        static let lat = "lat"
+        
+        /// "lon"
+        static let lon = "lon"
+        
+        /// "extras"
+        static let extras = "extras"
+        
+        /// "format"
+        static let format = "format"
+        
+        /// "nojsoncallback"
+        static let nojsoncallback = "nojsoncallback"
+    }
+    
+    /// Constants used as values in the url parameters
+    /// The API Key can be found at `Constants.FlickrAPIKey`
+    ///
+    /// These values include :
+    ///  * `searchMethod` = "flickr.photos.search"
+    ///  * `safeSearch` = 1
+    ///  * `extras` = "url_m"
+    ///  * `format` = "json"
+    ///  * `nojsoncallback` = 1
+    struct ParameterValues {
+        
+        /// "flickr.photos.search"
+        static let searchMethod = "flickr.photos.search"
+        
+        /// Integer: 1
+        static let safeSearch = 1
+        
+        /// "url_m"
+        static let extras = "url_m"
+        
+        /// "json"
+        static let format = "json"
+        
+        /// Integer: 1
+        static let nojsoncallback = 1
+    }
+    
+    /// Constants used to access values in the response object
+    struct ResponseKeys {
+        
+        /// The response object key to a image search using the method "flickr.photos.search"
+        /// Value: "photos"
+        static let photos = "photos"
+        
+        /// The total number of pictures matching the search
+        /// Value: "total"
+        static let total = "total"
+        
+        /// The key to the array of photo objects in the Flickr Response
+        /// Value: "photo"
+        static let photo = "photo"
+        
+        // In each photo object:
+        
+        /// The url defining the resulting image
+        /// Value: "url_m"
+        static let url_m = "url_m"
+    }
+}
--- VirtualTourist/VirtualTourist/FlickrClient.swift
+++ VirtualTourist/VirtualTourist/FlickrClient.swift
@@ -7,9 +7,174 @@
 //
 
 import Foundation
+import UIKit
 
 class FlickrClient {
     
+    // MARK: - Singleton
+    
+    /// A shared and threadsafe instance of FlickrClient
+    static let sharedInstance = FlickrClient()
+    
+    private init() {}
+    
+    // MARK: Search By Location
+    
+    func imageSearchByLocation() {
+        // get lat and long
+        let lat = 36.2
+        let lon = 81.7
+        
+        // get urls
+        taskForGet(latitude: lat, longitude: lon) { (data, error) in
+            if let error = error {
+                if error is ImageDownloadError {
+                    
+                } else if error is GetRequestError {
+                    
+                } else if error is URLRequestParseToJSONError {
+                    
+                }
+            } else {
+                guard let result = data as? [String: Any] else {
+                    let notDictionary = FlickrDeserializationError(kind: .resultNotDictionary, message: "Results could not be cast as the expected JSON object.", data: data)
+                    return
+                }
+                
+                guard let photosObject = result[ResponseKeys.photos] as? [String: Any] else {
+                    let noPhotosResult = FlickrDeserializationError(kind: .photosResultObjectNotResolvable, message: "Photos Object Does Not Exist.", data: result)
+                    return
+                }
+                
+                guard let photoArray = photosObject[ResponseKeys.photo] as? [[String: Any]] else {
+                    let photoObjectArrayDNE = FlickrDeserializationError(kind: .photoDictionaryDoesNotExist, message: "Dictionary of photo objects does not exist.", data: photosObject)
+                    return
+                }
+                
+                let array: [String] = photoArray.flatMap {
+                    guard let urlString = $0[FlickrClient.ResponseKeys.url_m] as? String else {
+                        return nil
+                    }
+                    return urlString
+                }
+             
+                // Do something with the array of urls
+                
+                
+            }
+        }
+    }
+    
+    // MARK: Task For GET
+    
+    func taskForGet(latitude: Double, longitude: Double, completionHandlerForGet: @escaping CompletionHandlerForGet) -> URLSessionDataTask {
+        let url = flickrUrl(latitude: latitude, longitude: longitude)
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            guard error == nil else {
+                let getError = GetRequestError(kind: .errorInResponse, message: "There was an error with your request.", url: url.absoluteString, errorMessage: "\(error!)")
+                completionHandlerForGet(nil, getError)
+                return
+            }
+            
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode < 300 else {
+                let getError = GetRequestError(kind: .non200Response, message: "Request returned something other than a 2xx response.", url: url.absoluteString, errorMessage: "")
+                completionHandlerForGet(nil, getError)
+                return
+            }
+            
+            guard let unwrappedData = data else {
+                let getError = GetRequestError(kind: .noData, message: "No Data was returned from your request!", url: url.absoluteString, errorMessage: "")
+                completionHandlerForGet(nil, getError)
+                return
+            }
+            self.convert(data: unwrappedData, with: completionHandlerForGet)
+        }
+        task.resume()
+        return task
+    }
+    
+    func convert(data: Data, with: CompletionHandlerForGet) {
+        var parsedResult: Any!
+        do {
+            parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+        } catch {
+            let deserializeError = URLRequestParseToJSONError(kind: .unparsableToJSON, message: "Could not parse the data as JSON.", data: data)
+            with(nil, deserializeError)
+            return
+        }
+        with(parsedResult, nil)
+    }
+    
+
+    
+    func fetchImageFor(url: URL,  completion: @escaping DownloadedImageHandler) {
+        
+    }
+}
+
+// MARK: - Convenience
+
+extension FlickrClient {
     
     
+    /// Builds a URL to request a latitude/longitude search from Flickr.
+    ///
+    /// - parameter latitude:  The latitude to search at.
+    /// - parameter longitude: The longitude to search at.
+    ///
+    /// - returns: A valid URL.
+    fileprivate func flickrUrl(latitude: Double, longitude: Double) -> URL {
+        let parameters: [String: Any] = [
+            ParameterKeys.method            : ParameterValues.searchMethod,
+            ParameterKeys.apiKey            : AppConstants.FlickrAPIKey,
+            ParameterKeys.safeSearch        : ParameterValues.safeSearch,
+            ParameterKeys.extras            : ParameterValues.extras,
+            ParameterKeys.format            : ParameterValues.format,
+            ParameterKeys.nojsoncallback    : ParameterValues.nojsoncallback,
+            ParameterKeys.bbox              : bboxString(latitude: latitude, longitude: longitude),
+            ParameterKeys.lat               : latitude,
+            ParameterKeys.lon               : longitude
+        ]
+        
+        return flickrUrlFrom(parameters: parameters)
+    }
+    
+    
+    /// Builds a valid `URL` instance from query parameters and url components.
+    ///
+    /// - parameter parameters: This is a `[String: Any]` dictionary that holds the key-value pairs that will be `URLQueryItems`.
+    ///
+    /// - returns: A valid URL.
+    fileprivate func flickrUrlFrom(parameters: [String: Any]) -> URL {
+        var components = URLComponents()
+        components.scheme = Constants.apiScheme
+        components.host = Constants.apiHost
+        components.path = Constants.apiPath
+        components.queryItems = [URLQueryItem]()
+        
+        for (key, value) in parameters {
+            let queryItem = URLQueryItem(name: key, value: "\(value)")
+            components.queryItems!.append(queryItem)
+        }
+        
+        return components.url!
+    }
+    
+    
+    /// `bboxString` creates a url-safe bounding box parameter.
+    ///
+    /// - parameter latitude:  The center coordinate's latitude of the bounding box
+    /// - parameter longitude: The center coordinate's longitude of the bounding box
+    ///
+    /// - returns: A string describing the bounding box ready to be passed as a url parameter.
+    fileprivate func bboxString(latitude: Double, longitude: Double) -> String {
+        let minLon = max(longitude - Constants.searchBBoxHalfWidth, Constants.searchLonRange.0)
+        let minLat = max(latitude - Constants.searchBBoxHalfHeight,Constants.searchLatRange.0)
+        let maxLon = min(longitude + Constants.searchBBoxHalfWidth,Constants.searchLonRange.1)
+        let maxLat = min(latitude + Constants.searchBBoxHalfHeight, Constants.searchLatRange.1)
+        return "\(minLon),\(minLat),\(maxLon),\(maxLat)"
+    }
+
 }
